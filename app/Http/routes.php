171c4b67b330cca376e9337b5adeb54ff8ec881c
Flame <flame.lin@gmail.com>
2016-05-11 17:59:18 +0800

    route 修正

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,21 +13,18 @@
 
 Route::resource('log', 'LogController');
 
-Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'), function () {
-	Route::get('logs', 'LogController@index');
-	Route::get('logs/author/{id}', 'LogController@author');
-});
-
 Route::group(array('middleware' => 'auth'), function () {
-	Route::get('/', 'LogController@create');
+    Route::get('/', 'LogController@create');
 });
 
 Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'manager'), function () {
-	Route::get('logs', 'LogController@index');
-	Route::get('users', ['as' => 'admin.user.index', 'uses' => 'UsersController@index']);
-	Route::get('roles', 'RolesController@index');
-	Route::get('roles/create', 'RolesController@create');
-	Route::post('roles/create', 'RolesController@store');
+    Route::get('logs', 'LogController@index');
+    Route::get('logs/author/{id}', 'LogController@author');
+
+    Route::get('users', 'UsersController@index');
+    Route::get('roles', 'RolesController@index');
+    Route::get('roles/create', 'RolesController@create');
+    Route::post('roles/create', 'RolesController@store');
 });
 
 Route::auth();
