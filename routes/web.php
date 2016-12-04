6c0bf427b24404199f159c06664c121a98bd2818
Flame@MBP <flame.lin@gmail.com>
2016-12-04 17:04:08 +0800

    fix: 更新 Lravel 至 5.3 版

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
- */
-
 Route::resource('log', 'LogsController');
 
 Route::group(array('middleware' => 'auth'), function () {
@@ -27,4 +16,5 @@ Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 
     Route::post('roles/create', 'RolesController@store');
 });
 
-Route::auth();
+Auth::routes();
+// Route::get('/home', 'HomeController@index');
